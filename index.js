098e393b79b7bf6de57504ff42a51a503fd3857f
Dilara Uluturhan <dilarauluturhan@outlook.com>
2023-04-01 22:12:29 +0300

    Some changes

--- index.js
+++ index.js
@@ -1,19 +1,54 @@
 /*
+Node.js bir derleyicidir.(compiler)
+Node.js sayesinde Javascript ile Server tarafında proje yazıp çıktı alabiliriz.
+Node.js ile doğrudan veritabanına bağlantı yapabiliriz.
 
-// request - response -> server
-// uygulamaya talep gelir ve talep sonucu response gönderirim
+request(talep) - response(yanıt) -> server
+uygulamaya talep gelir ve talep sonucu response gönderirim
+talebi alacak olan server'ı oluşturmalıyım
 
-// talebi alacak olan server'ı oluşturmalıyım
+request, res.end()'le biter ve bunu belli bir port altında hizmete açmam gerekir
+server.listen(3000, () => {
+    //mesaj
+});
 
+res.write() -> yanıt göndermek için
+
+node dosyaİsmi ile o anki halini çalıştırabilirim.
+
+bu kısmı özetleyecek olursak req metoduyla talep ettiğim özelliklerin, res metoduyla yanıtladığım özelliklerin çıktısı alınır.
+
+fs ile dosya oluşturma, okuma, isim değiştirme ve silmeye yarar.
+
+Express.js, node.js ile web sunucusuna gelen istekleri yönetmek, düzenlemek ve yayınlamak için kullanılır.
+
+*/
+
+/**********************************************************************/
+// npm init --yes -> package.json dosyası gelir
+// npm i express -> express.js için
+// npm install -> node_modules klasörü için
+// npm i nodemon -g -> sayfamdaki değişikliği aktif olarak görmek için(nodemon "dosyaAdi")
+// npm list -> projenin paketleri listelenir
+// npm list -g -> projenin global paketleri sıralanır
+// npm i ejs -> HTML sayfasına dinamik bir veri göndermek için
+/**********************************************************************/
+
+/*
+
+// http server'ı
+// require, kütüphaneyi projeye dahil eder
 var http = require("http");
 
 // File System
 // Dosyayla çalışırken bu modülü kullanıyoruz
 var fs = require("fs");
 
+// server oluşturuyorum
 var server = http.createServer((req, res) => {
 
     if (req.url == "/") {
+
         // index.html'i oku
         // içeriği callback fonk. ile çağıralım
         // okuma işlemi bittikten sonra birinci parametrede bir hata,ikinci parametrede de veri gelir, buna html dedim
@@ -24,7 +59,7 @@ var server = http.createServer((req, res) => {
         });
 
     } else if (req.url == "/products") {
-        fs.readFile("products.html", (err, html) => {
+        fs.readFile("products.html",  (err, html) => {
             res.write(html);
             res.end();
             // res.write("<h1>Products</h1>");
@@ -68,7 +103,7 @@ app.use("/products/:id", function (req, res) {
 
     // geriye döndürecek olduğum bir product bilgisi oluşturucam
     // kullanıcının göndermiş olduğu id bilgisine göre bunu döndürücem
-    // nasıl döndürücem? ren.render'a product ekleyerek...
+    // nasıl döndürücem? res.render'a product ekleyerek...
     const product = data.find(p => p.id == req.params.id);
 
     res.render("products-detail", product)
@@ -96,13 +131,3 @@ app.use("/", function (req, res) {
 app.listen(3000, () => {
     console.log("Listening on port 3000")
 });
-
-/**********************************************************************/
-// npm init --yes -> package.json dosyası gelir
-// npm i express -> express.js için
-// npm install -> node_modules klasörü için
-// npm i nodemon -g -> sayfamdaki değişikliği aktif olarak görmek için(nodemon "dosyaAdi")
-// npm list -> projenin paketleri listelenir
-// npm list -g -> projenin global paketleri sıralanır
-// npm i ejs -> HTML sayfasına dinamik bir veri göndermek için
-/**********************************************************************/
